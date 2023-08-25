782f38ba63cb7d2b4d4364f366ac38aa9bd21583
Mateo Ortiz <a22216355@alumnos.uady.mx>
2023-08-25 14:26:12 -0500

    c

--- main.cpp
+++ main.cpp
@@ -2,97 +2,51 @@
 
 typedef struct alumno
 {
-    char const *nombre;
+    std::string nombre;
     int creditos;
     int semestre;
-    struct alumno *siguiente;
 
-    void imprimirAlumno(void);
+    alumno(std::string nombre, int creditos, int semestre)
+    {
+        this->nombre = nombre;
+        this->creditos = creditos;
+        this->semestre = semestre;
+    }
+
+    void imprimirAlumno(void)
+    {
+        std::cout << "Nombre: " << this->nombre << ", Creditos: " << this->creditos << ", Semestre: " << this->semestre << std::endl;
+    }
 
 } Alumno;
+typedef Alumno *AlumnoPtr;
 
 typedef struct nodo
 {
-    struct alumno *alumnoPtr;
+    AlumnoPtr alumno;
     struct nodo *siguiente;
 
-    void imprimirLista();
-    int insertarNodoOrdenadoCreditos(Nodo *);
-} Nodo;
+    nodo(AlumnoPtr nuevo)
+    {
+        this->alumno = nuevo;
+        this->siguiente = NULL;
+    }
 
-// Prototipos de las funciones
-Alumno *crearAlumno(char const *, int, int);
-Nodo *crearNodo(Alumno *);
+    void imprimirLista(void){
+        NodoPtr actual=&this;
+
+        
+
+    }
+
+} Nodo;
+typedef Nodo *NodoPtr;
 
 int main(int argc, char const *argv[])
 {
-    Alumno *alumnos[5];
-    Nodo *listaAlumnos = nullptr;
+    AlumnoPtr alumnos[5];
 
-    alumnos[0] = crearAlumno("pepeOG", 100, 9);
-    alumnos[1] = crearAlumno("pepe2", 245, 9);
-    alumnos[2] = crearAlumno("pepe3", 300, 9);
-    alumnos[3] = crearAlumno("pepe4", 345, 9);
-    alumnos[4] = crearAlumno("pepe5", 400, 9);
-
-    // listaAlumnos = crearNodo(crearAlumno("Yoda", 400, 9));
-
-    // listaAlumnos->imprimirLista();
+    alumnos[0] = new Alumno("a", 1, 1);
 
     return 0;
-}
-
-Alumno *crearAlumno(char const *nombre, int creditos, int semestre)
-{
-    Alumno *nuevo = new Alumno;
-    nuevo->nombre = nombre;
-    nuevo->creditos = creditos;
-    nuevo->semestre = semestre;
-    nuevo->siguiente = nullptr;
-    return nuevo;
-}
-
-void Alumno::imprimirAlumno(void)
-{
-    std::cout << "Nombre: " << this->nombre << ", Creditos Aprovados: " << this->creditos << ", Semestre Equivalente: " << this->semestre << std::endl;
-}
-
-Nodo *crearNodo(Alumno *alumnoPtr)
-{
-    Nodo *nuevo = new Nodo;
-    nuevo->alumnoPtr = alumnoPtr;
-    nuevo->siguiente = nullptr;
-    return nuevo;
-}
-
-int nodo::insertarNodoOrdenadoCreditos(Nodo *nuevo)
-{
-    Alumno *anterior = nullptr, *actual = nullptr;
-    actual = this->alumnoPtr;
-
-    while (actual != nullptr && nuevo->alumnoPtr->creditos < actual->creditos)
-    {
-        anterior = actual;
-        actual = actual->siguiente;
-    }
-
-    if (anterior == this->alumnoPtr)
-    {
-        nuevo->siguiente = this;
-        this->alumnoPtr = nuevo;
-    }
-
-    return 0;
-}
-
-void nodo::imprimirLista()
-{
-    Alumno *actual = this->alumnoPtr;
-
-    std::cout << "\nImpresion de la lista de Alumnos (Ascendente)\n\n";
-    while (actual != nullptr)
-    {
-        actual->imprimirAlumno();
-        actual = actual->siguiente;
-    }
 }
