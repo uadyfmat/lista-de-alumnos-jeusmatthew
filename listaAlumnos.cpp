4067cc30ce7e3ff24aa3f08bbe1cf75ba7265404
Mateo Ortiz <jeusmateo@gmai.com>
2023-08-27 22:15:14 -0600

    añadido abstraccion de lista

--- listaAlumnos.cpp
+++ listaAlumnos.cpp
@@ -7,32 +7,59 @@ typedef struct alumno
     int creditos;
     int semestre;
 
+    alumno(char const *nombre, int creditos, int semestre)
+    {
+        this->nombre = nombre;
+        this->creditos = creditos;
+        this->semestre = semestre;
+        printf("Alumno creado en %p\n", this);
+    }
+
     void imprimirAlumno()
     {
         printf("Nombre: %s, Creditos: %d, Semestre: %d\n",
                this->nombre, this->creditos, this->semestre);
     }
 
-    void test(){
-        printf("Direccion de esto: %p\nDireccion dereferencia: %p\n", this, (*this));
-
-    }
-
 } Alumno;
+
+typedef Alumno *AlumnoPtr;
 
 typedef struct nodo
 {
     Alumno *alumnoPtr;
     struct nodo *siguiente;
 
+    nodo(AlumnoPtr alumno)
+    {
+        this->alumnoPtr = alumno;
+        this->siguiente = nullptr;
+        printf("Nodo creado en %p\n", this);
+    }
+
+} Nodo;
+
+typedef Nodo *NodoPtr;
+
+typedef struct lista
+{
+
+    NodoPtr cabeza;
+
+    lista(NodoPtr cabeza)
+    {
+        this->cabeza = cabeza;
+    }
+
     void imprimirLista()
     {
-        if(this==nullptr){
+        if (this == nullptr)
+        {
             printf("Lista vacia!");
             return;
         }
 
-        struct nodo* actual = this;
+        NodoPtr actual = this->cabeza;
         while (actual != nullptr)
         {
             actual->alumnoPtr->imprimirAlumno();
@@ -40,100 +67,56 @@ typedef struct nodo
         }
     }
 
-} Nodo;
+    void insertarNodoOrdenadoCreditos(AlumnoPtr nuevoAlumno)
+    {
+        NodoPtr anterior = nullptr,
+                actual = this->cabeza,
+                nuevo = new Nodo(nuevoAlumno);
 
-typedef struct nodo *NodoPtr;
-typedef struct alumno *AlumnoPtr;
+        while (actual != nullptr && nuevo->alumnoPtr->creditos < actual->alumnoPtr->creditos)
+        {
+            anterior = actual;
+            actual = actual->siguiente;
+        }
 
-// Prototipos de las funciones
-// Elementales
-Alumno *crearAlumno(char const *, int, int);
-// void imprimirAlumno(AlumnoPtr);
+        if (anterior == nullptr)
+        {
+            nuevo->siguiente = this->cabeza;
+            this->cabeza = nuevo;
+        }
+        else
+        {
+            anterior->siguiente = nuevo;
+            nuevo->siguiente = actual;
+        }
+    }
 
-NodoPtr crearNodo(AlumnoPtr);
-void insertarNodoOrdenadoCreditos(NodoPtr *, AlumnoPtr);
-// void imprimirLista(NodoPtr);
+} Lista;
 
-// Propias
+typedef Lista *ListaPtr;
 
 int main(int argc, char const *argv[])
 {
     // crear 5 instancias de estructuras Alumno
-    AlumnoPtr *alumnos = (AlumnoPtr *)malloc(sizeof(AlumnoPtr) * 5);
-
-    alumnos[0] = crearAlumno("Walter White", 100, 1);
-    alumnos[0]->test();
-    alumnos[1] = crearAlumno("Raul Magana", 200, 2);
-    alumnos[2] = crearAlumno("David Torres", 300, 3);
-    alumnos[3] = crearAlumno("Guadalupe Rios", 400, 4);
-    alumnos[4] = crearAlumno("Ml chispa", 500, 5);
+    AlumnoPtr alumnos[5];
+    alumnos[0] = new Alumno("Walter White", 100, 1);
+    alumnos[1] = new Alumno("Raul Magana", 200, 2);
+    alumnos[2] = new Alumno("David Torres", 300, 3);
+    alumnos[3] = new Alumno("Guadalupe Rios", 400, 4);
+    alumnos[4] = new Alumno("Ml chispa", 500, 5);
     // crear la lista con las 5 instancias de Alumno
-    NodoPtr cabeza = nullptr;
+    ListaPtr lista = new Lista(nullptr);
 
     // insertar de forma ordenada de acuerdo al número de créditos (descendente)
-    insertarNodoOrdenadoCreditos(&cabeza, alumnos[0]);
-    insertarNodoOrdenadoCreditos(&cabeza, alumnos[1]);
-    insertarNodoOrdenadoCreditos(&cabeza, alumnos[2]);
-    insertarNodoOrdenadoCreditos(&cabeza, alumnos[3]);
-    insertarNodoOrdenadoCreditos(&cabeza, alumnos[4]);
+    lista->insertarNodoOrdenadoCreditos(alumnos[0]);
+    lista->insertarNodoOrdenadoCreditos(alumnos[1]);
+    lista->insertarNodoOrdenadoCreditos(alumnos[2]);
+    lista->insertarNodoOrdenadoCreditos(alumnos[3]);
+    lista->insertarNodoOrdenadoCreditos(alumnos[4]);
 
     // imprimir la lista
     printf("\nImpresion de la lista descendente: \n");
-    cabeza->imprimirLista();
+    lista->imprimirLista();
 
     return 0;
 }
-
-// Elementales
-AlumnoPtr crearAlumno(char const *nombre, int creditos, int semestre)
-{
-    AlumnoPtr nuevo = (AlumnoPtr)malloc(sizeof(Alumno));
-
-    nuevo->nombre = nombre;
-    nuevo->creditos = creditos;
-    nuevo->semestre = semestre;
-
-    printf("Alumno creado en %p\n", nuevo);
-
-    return nuevo;
-}
-
-
-NodoPtr crearNodo(AlumnoPtr alumnoPtr)
-{
-    NodoPtr nuevo = (NodoPtr)malloc(sizeof(Nodo));
-    nuevo->alumnoPtr = alumnoPtr;
-    nuevo->siguiente = nullptr;
-
-    printf("Nodo creado en %p\n", nuevo);
-
-    return nuevo;
-}
-
-void insertarNodoOrdenadoCreditos(NodoPtr *cabeza, AlumnoPtr nuevoAlumno)
-{
-    NodoPtr anterior = nullptr,
-            actual = *cabeza,
-            nuevo = crearNodo(nuevoAlumno);
-
-    while (actual != nullptr && nuevo->alumnoPtr->creditos < actual->alumnoPtr->creditos)
-    {
-        anterior = actual;
-        actual = actual->siguiente;
-    }
-
-    if (anterior == nullptr)
-    {
-        nuevo->siguiente = *cabeza;
-        *cabeza = nuevo;
-    }
-    else
-    {
-        anterior->siguiente = nuevo;
-        nuevo->siguiente = actual;
-    }
-}
-
-
-
-// Propias
